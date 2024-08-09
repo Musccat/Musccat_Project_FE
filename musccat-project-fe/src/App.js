98a8d7e276f7d8b38a8a0520ef913a632582c812
judymoody59 <jmj.chai@gmail.com>
2024-08-09 10:36:41 +0900

    App.js 파일 수정

--- musccat-project-fe/src/App.js
+++ musccat-project-fe/src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import {
   BrowserRouter as
@@ -10,9 +11,20 @@ import styled from 'styled-components';
 
 //Pages
 import Login from './component/page/Login';
+import Register from './component/page/Register';
 import MainPage from './component/page/MainPage';
+import EntireScholar from './component/page/EntireScholar';
+import RecomScholar from './component/page/RecomScholar';
+import Notice from './component/page/Notice';
+import MyPage from './component/page/MyPage';
 
 
+const MainTitleText = styled.p`
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+
+`;
 
 
 function App(props) {
@@ -23,7 +35,12 @@ function App(props) {
         <Routes>
           <Route index element={<MainPage />} />
           <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
           <Route path="main" element={<MainPage />} />
+          <Route path="mypage" element={<MyPage />} />
+          <Route path="entirescholar" element={<EntireScholar />} />
+          <Route path="recomscholar" element={<RecomScholar />} />
+          <Route path="notice" element={<Notice />} />
         </Routes>
       </Router>
     </AuthProvider>
