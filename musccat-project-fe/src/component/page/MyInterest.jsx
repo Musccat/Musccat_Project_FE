415834bb18211269b759030c73c5ea4946b2e112
MinJu <108432112+judymoody59@users.noreply.github.com>
2024-09-05 15:43:34 +0900

    Feat: 마이페이지 수정1

--- musccat-project-fe/src/component/page/MyInterest.jsx
+++ musccat-project-fe/src/component/page/MyInterest.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import NavBar from '../ui/NavBar';
 import { useNavigate } from "react-router-dom";
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+    max-width: 800px;
+    margin: 50px auto;
+    padding: 20px;
+    border: 1px solid #348a8c;
+    border-radius: 8px;
+    background-color: #white;
+`;
 
 const MyInterest = () => {
     const navigate = useNavigate();
@@ -9,7 +20,9 @@ const MyInterest = () => {
     return (
         <>
         <NavBar/>
-        
+        <Container>
+
+        </Container>
         </>
     );
 };
