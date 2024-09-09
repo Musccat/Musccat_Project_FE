155ac79fe3f30764cbc4494e182971362a2136a0
MinJu <108432112+judymoody59@users.noreply.github.com>
2024-09-09 12:09:46 +0900

    Feat: 나의 관심목록1

---
+++ musccat-project-fe/src/component/data/interestdata.jsx
@@ -0,0 +1,8 @@
+const scholarships = [
+    {"foundation_name":"삼성꿈장학재단", "name":"리더 육성 장학사업", "recruitment_end":"24.06.13", "product_id": "008"},
+    {"foundation_name":"하동군장학재단", "name":"하동군 지역인재 장학사업", "recruitment_end":"24.06.15", "product_id": "009"},
+    {"foundation_name":"범석학술장학재단", "name":"범석 장학금", "recruitment_end":"24.06.20", "product_id": "010"}
+
+];
+
+export default scholarships;
--- musccat-project-fe/src/component/page/MyInterest.jsx
+++ musccat-project-fe/src/component/page/MyInterest.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import NavBar from '../ui/NavBar';
 import { useNavigate } from "react-router-dom";
 import styled from 'styled-components';
+import emptyheart from "../ui/emptyheart.jpeg";
+import filledheart from "../ui/filledheart.jpeg";
 
+const Wrapper = styled.div`
+    max-width: 800px;
+    margin: 50px auto; 
+    padding: 0 20px;
+`;
 
 const Container = styled.div`
-    max-width: 800px;
-    margin: 50px auto;
     padding: 20px;
     border: 1px solid #348a8c;
     border-radius: 8px;
     background-color: #white;
+`;
+
+const Heading = styled.h1`
+    margin: 0 0 20px 0;
+    text-align: left;
+    color: black;
 `;
 
 const MyInterest = () => {
@@ -20,9 +31,12 @@ const MyInterest = () => {
     return (
         <>
         <NavBar/>
-        <Container>
-
-        </Container>
+            <Wrapper>
+                <Heading>내 관심목록</Heading>
+                <Container>
+                    {/* Your table or other content goes here */}
+                </Container>
+            </Wrapper>
         </>
     );
 };
