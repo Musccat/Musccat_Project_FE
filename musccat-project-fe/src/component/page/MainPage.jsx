46661f6d5a90279def4d2abe6556e7278c38d8ce
judymoody59 <jmj.chai@gmail.com>
2024-10-02 05:08:56 +0900

    Feat: 메인페이지 디자인 구현

--- musccat-project-fe/src/component/page/MainPage.jsx
+++ musccat-project-fe/src/component/page/MainPage.jsx
@@ -5,6 +5,10 @@ import NavBar from "../ui/NavBar";
 import NavBarB from "../ui/NavBarB";
 import Button from "../ui/Button";
 import { useAuth } from '../contexts/AuthContext';
+import entirescholar_image from '../ui/scholli_entirescholarship_list.jpeg';
+import recomscholar_image from '../ui/scholli_recomscholarship_list.jpeg';
+import addscholar1 from '../ui/addscholar_image1.jpeg';
+import addscholar2 from '../ui/addscholar_image2.jpeg';
 
 const Wrapper = styled.div`
     padding: 16px;
@@ -35,18 +39,104 @@ const Rectangle = styled.div`
     border-radius: 8px;
     box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); /* 그림자 효과 */
     margin-bottom: 16px; /* 사각형 간의 간격 */
+    position: relative;
     display: flex;
-    flex-direction: column;
-    justify-content: flex-end; /* 하단 정렬 */
-    align-items: flex-start; /* 좌측 정렬 */
+    align-items: center;
+    justify-content: space-between;
+    overflow: hidden;
+`;
+
+const TextContainer = styled.div`
+    flex: 1;
+    margin-right: 20px;
+    z-index: 2;
+    padding-top: 30px;
 `;
 
 const Text = styled.h1`
     margin: 0;
-    padding-bottom: 20px; /* 버튼과의 간격을 위해 패딩 추가 */
+    padding-bottom: 20px; 
     font-size: 30px;
     color: #333;
 `;
+
+const Highlight = styled.span`
+    color: #348a8c; /* 원하는 색상 코드 */
+`;
+
+const GrayText = styled.p`
+    margin-top: 8px;
+    font-size: 20px; /* Adjust font size as needed */
+    color: #666; /* Gray color */
+    text-align: left; /* Align to the left or adjust as needed */
+`;
+
+const FirstImageWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    width: 40%;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    padding: 10px; 
+`;
+
+const FirstImage = styled.img`
+    width: 100%;
+    height: auto;
+    transform: scale(1.8) translate(-50px, -39px); ;
+    object-fit: contain;
+`;
+
+const SecondImageWrapper = styled.div`
+    position: absolute;
+    top: -50px; /* Adjust to position the image correctly */
+    left: -50px; /* Adjust as needed */
+    width: 200%; /* Scale the image to ensure only a part is visible */
+    height: auto;
+    z-index: 1; /* Behind the text */
+    opacity: 0.9; /* Optional: Make it slightly transparent */
+`;
+
+const SecondImage = styled.img`
+    width: 100%;
+    height: auto;
+    transform: scale(0.6) translate(-150px, -345px);
+`;
+
+const ThirdImageWrapper = styled.div`
+    position: absolute;
+    top: -50px; /* Adjust to position the image correctly */
+    left: -50px; /* Adjust as needed */
+    width: 200%; /* Scale the image to ensure only a part is visible */
+    height: auto;
+    z-index: 1; /* Behind the text */
+    opacity: 0.9; /* Optional: Make it slightly transparent */
+`;
+
+const ThirdImage1 = styled.img`
+    position: absolute;
+    width: 100%;
+    height: auto;
+    z-index: 2; /* Ensure it's on top */
+    transform: scale(0.2) translate(-700px, -1000px); /* Adjust position slightly for overlap */
+    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.8);
+    border-radius: 50px;
+`;
+
+const ThirdImage2 = styled.img`
+    position: absolute;
+    width: 90%;
+    height: auto;
+    z-index: 1; /* Ensure it's behind the first image */
+    opacity: 0.95; /* Optional: Add slight transparency */
+    transform: scale(0.25) translate(-450px, -500px);
+    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.8);
+    border-radius: 50px;
+`;
+
+
 
 const FirstButton = styled.button`
     padding: 8px 16px;
@@ -54,7 +144,7 @@ const FirstButton = styled.button`
     border-radius: 4px; /* Rounded corners for the button */
     background-color: #348a8c; 
     color: white; 
-    margin-top: auto;
+    margin-top: 90px;
     margin-left: 10px;
     cursor: pointer;
 `;
@@ -65,7 +155,7 @@ const SecondButton = styled.button`
     border-radius: 4px; /* Rounded corners for the button */
     background-color: #2f6877;
     color: white;
-    margin-top: auto;
+    margin-top: 60px;
     margin-left: 10px;
     cursor: pointer;
 `;
@@ -76,7 +166,7 @@ const ThirdButton = styled.button`
     border-radius: 4px; /* Rounded corners for the button */
     background-color: #2f4858;
     color: white;
-    margin-top: auto;
+    margin-top: 130px;
     margin-left: 10px;
     cursor: pointer;
 `;
@@ -95,6 +185,21 @@ const BottomButton = styled.button`
     font-size: 16px; /* 텍스트 크기 조정 */
     cursor: pointer;
 `;
+
+const WhiteText = styled.p`
+    color: white;
+    font-size: 12px; /* Adjust font size if necessary */
+    margin-top: 8px;
+    text-align: center; /* Adjust alignment if necessary */
+`;
+
+const LoginText = styled.p`
+    margin-top: 8px;
+    font-size: 10px; /* Adjust font size as needed */
+    color: #666; /* Use a slightly muted color for the text */
+    text-align: left;
+`;
+
 
 function MainPage(props) {
     const navigate = useNavigate();
@@ -131,8 +236,12 @@ function MainPage(props) {
                 {isAuthenticated ? (
                     <>
                     <Rectangle style={{ marginTop: "16px" }}>
+                        <TextContainer>
                         <Text>다양한 기관과 단체의
-                            <br/>장학금을 확인해보세요</Text>
+                            <br/><Highlight>장학금</Highlight>을 확인해보세요</Text>
+                        <GrayText>분산되어 있는 장학금 정보를
+                            <br/> 통합하여 제공합니다.
+                        </GrayText>
                         <Space />
                         <Space />
                         <Space />
@@ -143,6 +252,10 @@ function MainPage(props) {
                         >
                             전체 장학금 목록 보기
                         </FirstButton>
+                        </TextContainer>
+                        <FirstImageWrapper>
+                            <FirstImage src={entirescholar_image} alt="Scholarship example" />
+                        </FirstImageWrapper>
                         <Space />
                     </Rectangle>
                     <Space />
@@ -153,9 +266,13 @@ function MainPage(props) {
                     <Space />
                     <Space />
                     <Rectangle>
+                        <TextContainer>
                         <Text>나에게 맞는
-                            <br /> 맞춤 장학금을
+                            <br /> <Highlight>추천 장학금</Highlight>을
                             <br /> 확인해보세요. </Text>
+                        <GrayText>사용자 프로필을 분석하여
+                        <br/> 장학금을 추천해드립니다.
+                        </GrayText>
                         <Space />
                         <Space />
                         <Space />
@@ -165,6 +282,10 @@ function MainPage(props) {
                         >
                             추천 장학금 목록 보기
                         </SecondButton>
+                        </TextContainer>
+                        <SecondImageWrapper>
+                            <SecondImage src={recomscholar_image} alt="Scholarship example" />
+                        </SecondImageWrapper>
                         <Space />
                     </Rectangle>
                     <Space />
@@ -175,8 +296,11 @@ function MainPage(props) {
                     <Space />
                     <Space />
                     <Rectangle>
-                        <Text>장학재단
-                            <br />이신가요?</Text>
+                        <TextContainer>
+                        <Text><Highlight>장학재단</Highlight>이신가요?</Text>
+                        <GrayText>장학사업명을 직접
+                            <br/> 등록하실 수 있습니다.
+                        </GrayText>
                         <Space />
                         <Space />
                         <Space />
@@ -186,6 +310,11 @@ function MainPage(props) {
                         >
                             장학금 등록하기
                         </ThirdButton>
+                        </TextContainer>
+                        <ThirdImageWrapper>
+                            <ThirdImage1 src={addscholar1} alt="First overlapping image" />
+                            <ThirdImage2 src={addscholar2} alt="Second overlapping image" />
+                        </ThirdImageWrapper>
                         <Space />
                     </Rectangle>
 
@@ -193,11 +322,12 @@ function MainPage(props) {
                 ) : (
                     <>
                         <Rectangle style={{ marginTop: "16px" }}>
+                            <TextContainer>
                             <Text>다양한 기관과 단체의
-                                <br/>장학금을 확인해보세요</Text>
-                            <Space />
-                            <Space />
-                            <Space />
+                                <br/><Highlight>장학금</Highlight>을 확인해보세요</Text>
+                            <GrayText>분산되어 있는 장학금 정보를
+                                <br/> 통합하여 제공합니다.
+                            </GrayText>
                             <Space />
                             <Space />
                             <FirstButton
@@ -205,6 +335,11 @@ function MainPage(props) {
                             >
                                 전체 장학금 목록 보기
                             </FirstButton>
+                            <LoginText>* 로그인 후 이용하실 수 있는 서비스입니다.</LoginText>
+                            </TextContainer>
+                            <FirstImageWrapper>
+                                <FirstImage src={entirescholar_image} alt="Scholarship example" />
+                            </FirstImageWrapper>
                             <Space />
                         </Rectangle>
                         <Space />
@@ -215,9 +350,13 @@ function MainPage(props) {
                         <Space />
                         <Space />
                         <Rectangle>
+                            <TextContainer>
                             <Text>나에게 맞는
-                                <br /> 맞춤 장학금을
+                                <br /> <Highlight>추천 장학금</Highlight>을
                                 <br /> 확인해보세요. </Text>
+                            <GrayText>사용자 프로필을 분석하여
+                            <br/> 장학금을 추천해드립니다.
+                            </GrayText>
                             <Space />
                             <Space />
                             <Space />
@@ -227,6 +366,11 @@ function MainPage(props) {
                             >
                                 추천 장학금 목록 보기
                             </SecondButton>
+                            <LoginText>* 로그인 후 이용하실 수 있는 서비스입니다.</LoginText>
+                            </TextContainer>
+                            <SecondImageWrapper>
+                                <SecondImage src={recomscholar_image} alt="Scholarship example" />
+                            </SecondImageWrapper>
                             <Space />
                         </Rectangle>
                         <Space />
@@ -237,7 +381,11 @@ function MainPage(props) {
                         <Space />
                         <Space />
                         <Rectangle>
-                            <Text>장학재단이신가요?</Text>
+                            <TextContainer>
+                            <Text><Highlight>장학재단</Highlight>이신가요?</Text>
+                            <GrayText>장학사업명을 직접
+                                <br/> 등록하실 수 있습니다.
+                            </GrayText>
                             <Space />
                             <Space />
                             <Space />
@@ -247,6 +395,12 @@ function MainPage(props) {
                             >
                                 장학금 등록하기
                             </ThirdButton>
+                            <LoginText>* 로그인 후 이용하실 수 있는 서비스입니다.</LoginText>
+                            </TextContainer>
+                            <ThirdImageWrapper>
+                                <ThirdImage1 src={addscholar1} alt="First overlapping image" />
+                                <ThirdImage2 src={addscholar2} alt="Second overlapping image" />
+                            </ThirdImageWrapper>
                             <Space />
                         </Rectangle>
                     </>
@@ -264,6 +418,9 @@ function MainPage(props) {
                 >
                     장학 수혜 정보 입력하고{"\n"}포인트 받기
                 </BottomButton>
+                {!isAuthenticated && (
+                    <WhiteText>* 로그인 후 이용하실 수 있는 서비스입니다.</WhiteText>
+                )}
                 <Space />
                 <Space />
                 <Space />
